37e54c265904adc10e7ce032a389a0d573de77fc
Vithanage N.Y.P <It25100548@my.Sliit.lk>
2026-03-07 05:10:47 +0530

    add dashboard service to service

---
+++ src/service/DashboardService.java
@@ -0,0 +1,15 @@
+package service;
+
+public class DashboardService {
+    public int getTotalEvents() {
+        return 10;   // this values for test later i will add text files then i will update this
+    }
+
+    public int getTotalUsers() {
+        return 20;
+    }
+
+    public int getTotalBookings() {
+        return 35;
+    }
+}
